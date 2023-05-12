b34389974b8b9fdd38938fc87413358c2269dccb
Tom Brookes <tom.brookes15@gmail.com>
2023-05-12 16:28:25 +0100

    API connected to index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,22 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    query = params[:query].presence || " "
+    response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['API_KEY']}&query=#{query}")
+    @results = response.parsed_response
+  end
+
+  def show
+    if params[:id] == 'search'
+      @movies = Movie.search(params[:query])
+      render 'index'
+    else
+      @movie = Movie.find(params[:id])
+    end
   end
 
   def search
     query = params[:query]
-    @results = Theomdb::Api.search(query)
+    @results = Tmdb::Movie.find(query)
     respond_to do |format|
       format.js # render a JavaScript response for AJAX requests
     end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,9 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["query", "results"]
 
-  async search() {
-    const response = await fetch(`/movies/search?query=${this.queryTarget.value}`, { headers: { accept: "application/javascript" } })
-    const html = await response.text()
-    this.resultsTarget.innerHTML = html
+  connect() {
+    console.log("connected")
   }
+
+  // async search() {
+  //   const response = await fetch(`/movies/search?query=${this.queryTarget.value}`, { headers: { accept: "application/javascript" } })
+  //   const html = await response.text()
+  //   this.resultsTarget.innerHTML = html
+  // }
 }
