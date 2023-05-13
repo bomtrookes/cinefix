0458d9a27708ca75ae048b13fef3dc21c3a1e824
Tom Brookes <tom.brookes15@gmail.com>
2023-05-14 00:07:51 +0100

    rating controller

--- db/migrate/20230512233007_create_ratings.rb
+++ db/migrate/20230512233007_create_ratings.rb
@@ -1,7 +1,16 @@
 class CreateRatings < ActiveRecord::Migration[7.0]
   def change
-    create_table :ratings do |t|
-
+    create_table :rankings do |t|
+      t.integer :story
+      t.integer :acting
+      t.integer :dialog
+      t.integer :cinematography
+      t.integer :soundtrack
+      t.integer :style
+      t.integer :pacing
+      t.integer :originality
+      t.integer :characters
+      t.integer :enjoyment
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_184001) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_233007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_184001) do
     t.text "synopsis"
     t.string "poster_url"
     t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rankings", force: :cascade do |t|
+    t.integer "story"
+    t.integer "acting"
+    t.integer "dialog"
+    t.integer "cinematography"
+    t.integer "soundtrack"
+    t.integer "style"
+    t.integer "pacing"
+    t.integer "originality"
+    t.integer "characters"
+    t.integer "enjoyment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
