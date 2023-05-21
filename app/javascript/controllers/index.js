ccbdff375cd20a566ba2d2f0e5a5ab8c5e419822
Tom Brookes <tom.brookes15@gmail.com>
2023-05-21 15:21:01 +0100

    mobile navbar implemented

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import NavigationController from "./navigation_controller"
+application.register("navigation", NavigationController)
+
 import TabsController from "./tabs_controller"
 application.register("tabs", TabsController)
