467a20420699e9202310562f3f300d91daffea5b
Tom Brookes <tom.brookes15@gmail.com>
2023-05-21 16:26:15 +0100

    update movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,8 @@
 class Movie < ApplicationRecord
-
   has_many :users
-
-  accepts_nested_attributes_for :ratings
 
   validates :title, presence: true
   validates :year, presence: true
   attribute :genres, :string, array: true
-
-
-  def self.with_ratings(user)
-    joins(:ratings).where("ratings.user_id = ? AND NOT rating IS NULL", user.id).distinct
-  end
-
 
 end
