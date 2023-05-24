aa39188886698a99d75bd7acad4b398bdb0a0f1e
Tom Brookes <tom.brookes15@gmail.com>
2023-05-24 19:17:56 +0100

    add/remove button working

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -5,30 +5,22 @@ class WatchlistsController < ApplicationController
   end
 
   def create
-    @watchlist = current_user.watchlist
+    @watchlist = current_user.watchlists
     @movie = Tmdb::Movie.detail(params[:id])
+    @watchlist = current_user.watchlists.build(api_id: params[:id])
+    @watchlist.save
 
-    if @watchlist.movies.include?(@movie)
-      flash[:notice] = "Movie is already in your watchlist"
-    else
-      @watchlist.movies << @movie
-      flash[:success] = "Movie added to watchlist"
-    end
-    redirect_to root_path
+    session[:return_to] ||= request.referer
+    redirect_to session.delete(:return_to)
+
   end
 
   def destroy
-    # watchlists??
-    @watchlist = current_user.watchlists
-    @movie = Tmdb::Movie.detail(params[:id])
+    @watchlist = Watchlist.find_by(api_id: params[:id])
+    @watchlist.destroy
 
-    if @watchlist.movies.include?(@movie)
-      @watchlist.movies.delete(@movie)
-      flash[:success] = "Movie removed from watchlist"
-    else
-      flash[:notice] = "Movie is not in your watchlist"
-    end
-    redirect_to root_path
+    session[:return_to] ||= request.referer
+    redirect_to session.delete(:return_to)
   end
 
   private
