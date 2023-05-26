f59ed5154de5f128c1987b57056b4c1493057862
Tom Brookes <tom.brookes15@gmail.com>
2023-05-26 23:41:32 +0100

    user and follow model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   has_many :watchlists
   has_many :movies, through: :ratings
 
+  has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+  has_many :followers, through: :received_follows, source: :follower
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followings, through: :given_follows, source: :followed_user
+
   def movie_ratings
     MovieRating.joins(rating: :movie).where(ratings: { user_id: self.id })
   end
