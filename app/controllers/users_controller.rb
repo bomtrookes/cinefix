800ac7eaffd95cb3fc8ea637befb75f328ca4936
Tom Brookes <tom.brookes15@gmail.com>
2023-05-27 12:24:52 +0100

    User views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    if current_user.id != params[:id].to_i
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
+  end
+
   def edit
     @user = current_user
   end
