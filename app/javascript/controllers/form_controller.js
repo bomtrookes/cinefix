4db57beb5022f22074f86e49fd322db800a72386
Tom Brookes <tom.brookes15@gmail.com>
2023-05-28 14:11:44 +0100

    slider update

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "slider" ]
+
+  connect() {
+    console.log("Form controller connected.")
+  }
+
+  update() {
+    this.sliderTargets.forEach((slider) => {
+      slider.labels[0].firstElementChild.innerText = slider.value
+    })
+  }
+}
