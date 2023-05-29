dae1035a9aebd18527e7adccd9fe4622135f4e89
Tom Brookes <tom.brookes15@gmail.com>
2023-05-29 22:33:06 +0100

    delete ratings, redirect to profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'movies#home'
 
   resources :users, only: [:index, :show] do
+    resources :ratings, only: [:destroy]
     resources :follows, only: [:create, :destroy]
     get 'following', action: :following, controller: 'follows'
     get 'followers', action: :followers, controller: 'follows'
