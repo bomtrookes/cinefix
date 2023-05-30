ce1ede5bc9d65284bc4b289f7b8632016d2e7c3d
Tom Brookes <tom.brookes15@gmail.com>
2023-05-30 17:35:31 +0100

    signup with username and avatar included

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   Tmdb::Api.key(ENV["API_KEY"])
+
+
+  def handle_routing_error
+    render plain: "404 Not Found", status: :not_found
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+  end
+
 end
