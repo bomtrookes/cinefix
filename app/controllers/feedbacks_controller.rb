a9ae52b5d724f85245c87d7045ae1197051d3631
Tom Brookes <tom.brookes15@gmail.com>
2023-05-31 13:09:25 +0100

    feedback controller methods

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,25 @@
 class FeedbacksController < ApplicationController
+
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+
+    if @feedback.save
+      flash[:notice] = "Your message has been sent!"
+      FeedbackMailer.feedback_email(@feedback).deliver_now
+      redirect_to root_path, notice: "Message sent successfully!"
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
