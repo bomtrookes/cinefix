a0158d577a3ac4e449371f1be9a0a0312d0c0331
Tom Brookes <tom.brookes15@gmail.com>
2023-06-15 23:32:11 +0100

    fixed stimuls issue with 'rails assets:precompile'

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -5,37 +5,37 @@ export default class extends Controller {
 
   connect() {
     console.log("Tabs Controller Connected")
-    this.threeTarget.classList.add("hidden");
-    this.twoTarget.classList.add("hidden");
+    this.threeTarget.classList.add("hidden")
+    this.twoTarget.classList.add("hidden")
   }
   showOne() {
     console.log("showOne")
-    this.oneTarget.classList.remove("hidden");
-    this.oneTabTarget.classList.add("selected");
+    this.oneTarget.classList.remove("hidden")
+    this.oneTabTarget.classList.add("selected")
 
-    this.twoTarget.classList.add("hidden");
-    this.twoTabTarget.classList.remove("selected");
-    this.threeTarget.classList.add("hidden");
-    this.threeTabTarget.classList.remove("selected");
+    this.twoTarget.classList.add("hidden")
+    this.twoTabTarget.classList.remove("selected")
+    this.threeTarget.classList.add("hidden")
+    this.threeTabTarget.classList.remove("selected")
   }
   showTwo() {
     console.log("showTwo")
-    this.twoTarget.classList.remove("hidden");
-    this.twoTabTarget.classList.add("selected");
+    this.twoTarget.classList.remove("hidden")
+    this.twoTabTarget.classList.add("selected")
 
-    this.oneTarget.classList.add("hidden");
-    this.threeTarget.classList.add("hidden");
-    this.oneTabTarget.classList.remove("selected");
-    this.threeTabTarget.classList.remove("selected");
+    this.oneTarget.classList.add("hidden")
+    this.threeTarget.classList.add("hidden")
+    this.oneTabTarget.classList.remove("selected")
+    this.threeTabTarget.classList.remove("selected")
   }
   showThree() {
     console.log("showThree")
-    this.threeTarget.classList.remove("hidden");
-    this.threeTabTarget.classList.add("selected");
+    this.threeTarget.classList.remove("hidden")
+    this.threeTabTarget.classList.add("selected")
 
-    this.oneTarget.classList.add("hidden");
-    this.twoTarget.classList.add("hidden");
-    this.oneTabTarget.classList.remove("selected");
-    this.twoTabTarget.classList.remove("selected");
+    this.oneTarget.classList.add("hidden")
+    this.twoTarget.classList.add("hidden")
+    this.oneTabTarget.classList.remove("selected")
+    this.twoTabTarget.classList.remove("selected")
   }
 }
